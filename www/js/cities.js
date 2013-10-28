17525c2575078186338079ae2f2fb059f20e5dab
BromleyJohn <john@fleetfacts.net>
2013-10-28 18:09:32 +0000

    init

--- www/js/cities.js
+++ www/js/cities.js
@@ -1,21 +1,21 @@
- //$(document).ready(function(){
-document.addEventListener("deviceready", function(){
-    alert("U R here!!!"); 
+ $(document).ready(function(){
+//document.addEventListener("deviceready", function(){
+  //  alert("U R here!!!"); 
 
-   // $.ajax({ // ajax call starts
+    $.ajax({ // ajax call starts
         
-   //      url: 'http://johnah.byethost7.com/cities.php?callback=?', // JQuery loads serverside.php
+         url: 'http://johnah.byethost7.com/cities.php?callback=?', // JQuery loads serverside.php
         
         //url: 'cities.php', // JQuery loads serverside.php
-  //	      dataType: 'jsonp', // Choosing a JSON datatype json for local jasonp for crossweb - NOT SURE ABOUT THIS!!!
-  //        success: function(data) // Variable data contains the data we get from serverside
-  //        {
-  //        $('#cities').html(''); // Clear #bands div
-   //       for (var i in data) {
-   //          $('#cities').append(data[i] + '<br/>');
-   //       }
+  	      dataType: 'jsonp', // Choosing a JSON datatype json for local jasonp for crossweb - NOT SURE ABOUT THIS!!!
+          success: function(data) // Variable data contains the data we get from serverside
+          {
+          $('#cities').html(''); // Clear #bands div
+          for (var i in data) {
+             $('#cities').append(data[i] + '<br/>');
+          }
 
-   //     }
-    //});
+        }
+    });
    // return false; 
 });
